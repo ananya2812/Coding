8c262f96d2ec7fc1ffa5df1d45672dccbf78a0a2
Ananya Sen <ananya.sen@infoedge.com>
2020-07-25 14:57:37 +0530

    Insertion and traversal code

--- CircularLinkedList/src/Basic/Traversal.java
+++ CircularLinkedList/src/Basic/Traversal.java
@@ -2,4 +2,50 @@ package Basic;
 
 public class Traversal {
 
+  public static void main(String[] args) {
+    Node head = null;
+    head = push(head, 12);
+    head = push(head, 56);
+    head = push(head, 2);
+    head = push(head, 11);
+    System.out.println("Contents of Circular " + "Linked List:");
+    printList(head);
+  }
+
+  static class Node {
+    Node next;
+    int data;
+
+    Node() {}
+
+    Node(int data, Node next) {
+      this.data = data;
+      this.next = next;
+    }
+  }
+
+  public static Node push(Node headNode, int data) {
+    Node newNode = new Node(data, headNode);
+    Node tempNode = headNode;
+    if (null != headNode) {
+      while (tempNode.next != headNode) {
+        tempNode = tempNode.next;
+      }
+      tempNode.next = newNode;
+    } else {
+      newNode.next = newNode;
+    }
+    return newNode;
+  }
+
+  public static void printList(Node head) {
+    if (head != null) {
+      Node temp = head;
+      do {
+        System.out.println(temp.data);
+        temp = temp.next;
+      } while (head != temp);
+    }
+
+  }
 }
