2a8cb7d575f5432d06b671c1d22d041b76c0c168
Ananya Sen <ananya.sen@infoedge.com>
2020-07-25 17:38:25 +0530

    Splitting Circular list into 2 parts ..................

--- CircularLinkedList/src/Easy/SortedInsert.java
+++ CircularLinkedList/src/Easy/SortedInsert.java
@@ -1,5 +1,74 @@
 package Easy;
 
+/*
+ * Write a function to insert a new value in a sorted Circular Linked List (CLL)
+ */
 public class SortedInsert {
 
+  public static void main(String[] args) {
+    LinkedList list = new LinkedList();
+    int arr[] = new int[] {12, 56, 2, 11, 1, 90};
+    Node temp = null;
+
+    for (int i = 0; i < 6; i++) {
+      temp = new Node(arr[i]);
+      list.sortedInsert(temp);
+    }
+
+    list.printList();
+  }
+
+  static class Node {
+    int data;
+    Node next;
+
+    Node(int d) {
+      data = d;
+      next = null;
+    }
+  }
+
+  static class LinkedList {
+    Node head;
+
+    LinkedList() {
+      head = null;
+    }
+
+    void printList() {
+      if (head != null) {
+        Node temp = head;
+        do {
+          System.out.print(temp.data + " ");
+          temp = temp.next;
+        } while (temp != head);
+      }
+    }
+
+    void sortedInsert(Node newNode) {
+      Node currNode = head;
+      // List is empty
+      if (null == currNode) {
+        newNode.next = newNode;
+        head = newNode;
+        return;
+      } // New Node is to be inserted at beginning
+      else if (currNode.data > newNode.data) {
+        while (currNode.next != head) {
+          currNode = currNode.next;
+        }
+        currNode.next = newNode;
+        newNode.next = head;
+        head = newNode;
+        return;
+      } // New Node is to be inserted after some node
+      else {
+        while (currNode.next != head && newNode.data > currNode.next.data) {
+          currNode = currNode.next;
+        }
+        newNode.next = currNode.next;
+        currNode.next = newNode;
+      }
+    }
+  }
 }
--- CircularLinkedList/src/Easy/SplitInTwoHalves.java
+++ CircularLinkedList/src/Easy/SplitInTwoHalves.java
@@ -1,5 +1,79 @@
 package Easy;
 
+/*
+ * Split a Circular Linked List into two halves If there are odd number of nodes, then first list
+ * should contain one extra.
+ */
 public class SplitInTwoHalves {
+
+  static Node head, head1, head2;
+
+  public static void main(String[] args) {
+    head = new Node(12, null);
+    head.next = new Node(56, null);
+    head.next.next = new Node(2, null);
+    head.next.next.next = new Node(11, null);
+    head.next.next.next.next = new Node(13, null);
+    head.next.next.next.next.next = head;
+
+    System.out.println("Original Circular Linked list ");
+    printList(head);
+    System.out.println("");
+    splitList();
+    System.out.println("First Circular List ");
+    printList(head1);
+    System.out.println("");
+    System.out.println("Second Circular List ");
+    printList(head2);
+  }
+
+  static class Node {
+    int data;
+    Node next;
+
+    Node() {}
+
+    Node(int data, Node next) {
+      this.data = data;
+      this.next = next;
+    }
+  }
+
+  private static void printList(Node node) {
+    Node temp = node;
+    if (node != null) {
+      do {
+        System.out.print(temp.data + " ");
+        temp = temp.next;
+      } while (temp != node);
+    }
+  }
+
+  private static void splitList() {
+    if (head == null) {
+      return;
+    }
+    Node fastPtr = head, slowPtr = head;
+    while (fastPtr.next != head && fastPtr.next.next != head) {
+      fastPtr = fastPtr.next.next;
+      slowPtr = slowPtr.next;
+    }
+    // Case for even Nodes
+    if (fastPtr.next.next == head) {
+      fastPtr = fastPtr.next;
+    }
+
+    // Make Heads of 2 lists
+    head1 = head;
+    if (head.next != head) {
+      head2 = slowPtr.next;
+    }
+
+    // Make 2nd half circular
+    fastPtr.next = slowPtr.next;
+
+    // Make 1st half circular
+    slowPtr.next = head;
+  }
 
 }
